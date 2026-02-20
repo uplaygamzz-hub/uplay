97963d0245e17a0e996bccf8899dc67352490b40
Austine Victor <157591916+Austin-Victor@users.noreply.github.com>
2026-02-20 19:08:15 +0100

    Delete Uplay-backend directory

--- Uplay-backend/tournaments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.tournament_list, name='tournament_list'),
-    path('join/<int:tournament_id>/', views.join_tournament, name='join_tournament'),
-]
